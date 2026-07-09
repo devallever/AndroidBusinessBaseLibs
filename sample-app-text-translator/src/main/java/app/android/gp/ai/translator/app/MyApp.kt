fdcac9376c667c22b1f6d233d5e2c21b927b25e5
devallever <devallever@163.com>
2026-07-09 11:50:19 +0800

    FEATURE: sample-app-text-translator-优化启动

--- sample-app-text-translator/src/main/java/app/android/gp/ai/translator/app/MyApp.kt
+++ sample-app-text-translator/src/main/java/app/android/gp/ai/translator/app/MyApp.kt
@@ -3,6 +3,7 @@ package app.android.gp.ai.translator.app
 import android.content.ClipboardManager
 import android.content.Context
 import app.allever.android.lib.core.app.App
+import app.allever.android.lib.core.ext.log
 import app.android.gp.ai.translator.function.SettingHelper
 import app.android.gp.ai.translator.translate.TranslationHelper
 import app.android.gp.ai.translator.ui.DialogTranslatePage
@@ -10,7 +11,11 @@ import app.allever.android.lib.core.helper.ActivityHelper
 import org.litepal.LitePal
 
 object MyApp {
+    var isInit = false
     fun onCreate() {
+        if (isInit) {
+             return
+        }
         TranslationHelper.init(App.context)
         
 
@@ -20,10 +25,13 @@ object MyApp {
         
         val clipBoardManager = App.context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
         clipBoardManager.addPrimaryClipChangedListener {
+            log( "收到复制事件")
             if (ActivityHelper.size() == 0 && SettingHelper.getAutoTranslate()) {
                 val srcText = clipBoardManager.primaryClip?.getItemAt(0)?.text.toString()
                 DialogTranslatePage.start(App.context, srcText)
             }
         }
+
+        isInit = true
     }
 }
